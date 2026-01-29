aea1ce7d9ca15c1becb18b2de2d6882b6f1809ef
doitchuu <seulgichu14@gmail.com>
2026-01-30 08:29:35 +0900

    Time: 2 ms (68.98%), Space: 60.2 MB (76.51%) - LeetHub

--- 0046-permutations/0046-permutations.ts
+++ 0046-permutations/0046-permutations.ts
@@ -1,58 +1,27 @@
-// nums = [1, 2, 3];
-
 function permute(nums: number[]): number[][] {
-    const answer = [];
-    const isUsed = new Array(nums.length).fill(false);
+    const isVisited = new Array(nums.length).fill(false);
+    const result = [];
 
-    // 정답을 만드는 함수
-    const recursion = (isUsed, currentList) => { // [t, t, t], [1, 2, 3]
-        if (currentList.length === nums.length) {
-            answer.push(currentList);
-
+    function dfs(arr) {
+        if (arr.length === nums.length) {
+            result.push([...arr]);
             return;
         }
 
         for (let i = 0; i < nums.length; i++) {
-            const currentValue = nums[i];
+            if (isVisited[i]) continue;
+            
+            isVisited[i] = true;
+            arr.push(nums[i]);
 
-            if (isUsed[i]) {
-                continue;
-            }
+            dfs(arr);
 
-            isUsed[i] = true;
-
-            recursion(isUsed, [...currentList, currentValue]);
-
-            isUsed[i] = false;
+            arr.pop();
+            isVisited[i] = false;
         }
-    };
+    }
 
-    recursion(isUsed, []);
+    dfs([]);
 
-    return answer;
+    return result;
 };
-
-// [1, 2, 3, 4]
-
-// 1. 현재 자릿수에 어떤걸 넣을 수 있는가?
-// 2. 썼던 수는 못쓴다
-
-// 1. 첫번째 자릿수에 어떤 수를 넣을까
-// 2. [1, 2, 3]중에 안쓴 수가 뭐가 있을까
-// 3. 1을 안썼네 1을 넣자.
-
-// 4. 두번째 자릿수에 어떤 수를 넣을까
-// 5. [1, 2, 3]중에 안쓴 수가 뭐가 있을까
-// 6. 2를 안썼네 2를 넣자
-
-// 7. 세번째 자릿수에 어떤 수를 넣을까
-// 8. [1, 2, 3]중에 안쓴 수가 뭐가 있을까
-// 9. 3를 안썼네 3를 넣자
-
-// 7. 네번째 자릿수에 어떤 수를 넣을까
-// 8. [1, 2, 3]중에 안쓴 수가 뭐가 있을까
-// 9. 3를 안썼네 3를 넣자
-
-
-// // 현재 자릿수에 넣을 수 있는 수 판단 -> isUsed
-// // 자릿수에 넣고, 다음 자릿수로 이동
