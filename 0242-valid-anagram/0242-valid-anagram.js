935b64a4c550b2cb94844a96c07ca724dff81906
doitchuu <seulgichu14@gmail.com>
2026-02-07 23:01:58 +0900

    Time: 14 ms (56.89%), Space: 56.4 MB (66.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,20 +3,34 @@
  * @param {string} t
  * @return {boolean}
  */
-function isAnagram(s, t) {
-  let comparedString = t;
-  
-    for (let i = 0; i < s.length; i++) {
-      if (comparedString.indexOf(s[i]) === -1) {
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  const max = Math.max(s.length, t.length);
+  const sMap = new Map();
+  const tMap = new Map();
+
+  for (let i = 0; i < max; i++) {
+    if (s[i]) {
+      sMap.set(s[i], (sMap.get(s[i]) ?? 0) + 1);
+    }
+    if (t[i]) {
+      tMap.set(t[i], (tMap.get(t[i]) ?? 0) + 1);
+    }
+  }
+
+  if (sMap.size !== tMap.size) {
+    return false;
+  }
+
+  for (const [char, count] of sMap) {
+    if (tMap.get(char) !== count) {
         return false;
-      }
-      
-      comparedString = comparedString.replace(s[i], "");
     }
+  }
 
-    if (comparedString.length !== 0) {
-      return false;
-    }
-
-    return true;
+  return true;
 };
