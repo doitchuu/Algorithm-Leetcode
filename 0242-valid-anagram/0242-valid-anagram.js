e41657388a9e4f8df4082fc2abcb675bad187f48
doitchuu <seulgichu14@gmail.com>
2026-02-17 01:34:30 +0900

    Time: 13 ms (63.36%), Space: 57.4 MB (55.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,34 +3,30 @@
  * @param {string} t
  * @return {boolean}
  */
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isAnagram = function(s, t) {
-  const max = Math.max(s.length, t.length);
-  const sMap = new Map();
-  const tMap = new Map();
-
-  for (let i = 0; i < max; i++) {
-    if (s[i]) {
-      sMap.set(s[i], (sMap.get(s[i]) ?? 0) + 1);
-    }
-    if (t[i]) {
-      tMap.set(t[i], (tMap.get(t[i]) ?? 0) + 1);
-    }
-  }
-
-  if (sMap.size !== tMap.size) {
-    return false;
-  }
-
-  for (const [char, count] of sMap) {
-    if (tMap.get(char) !== count) {
+    if (s.length !== t.length) {
         return false;
     }
-  }
 
-  return true;
+    const sMap = new Map();
+    const tMap = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i], (sMap.get(s[i]) ?? 0) + 1);
+        tMap.set(t[i], (tMap.get(t[i]) ?? 0) + 1);
+    }
+
+    for (const [key, value] of sMap) {
+        const tValue = tMap.get(key);
+
+        if (tValue === undefined) {
+            return false;
+        }
+
+        if (tValue !== value) {
+            return false;
+        }
+    }
+
+    return true;
 };
