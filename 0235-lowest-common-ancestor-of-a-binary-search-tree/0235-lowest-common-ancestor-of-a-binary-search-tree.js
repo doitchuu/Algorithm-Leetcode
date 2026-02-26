b22abd91067f70ac9a01337b62fee4ebf28f08f2
doitchuu <seulgichu14@gmail.com>
2026-02-26 23:50:24 +0900

    Time: 50 ms (95.77%), Space: 65.1 MB (70.21%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,18 +13,18 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-  const small = Math.min(p.val, q.val);
-  const big = Math.max(p.val, q.val);
+    const big = Math.max(p.val, q.val);
+    const small = Math.min(p.val, q.val);
 
-  while (root) {
-    if (big < root.val) {
-      root = root.left;
-    } else if (small > root.val) {
-      root = root.right;
-    } else {
-      return root;
+    while (root) {
+        if (big < root.val) {
+            root = root.left;
+        } else if (small > root.val) {
+            root = root.right;
+        } else {
+            return root;
+        }
     }
-  }
 
-  return null;
+    return null;
 };
