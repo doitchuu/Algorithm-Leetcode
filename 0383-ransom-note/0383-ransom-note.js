b5c9c5e58568dfff1e381d864eec669f5c245c0d
doitchuu <seulgichu14@gmail.com>
2026-03-02 15:45:53 +0900

    Time: 5 ms (97.78%), Space: 55.6 MB (93.7%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -3,18 +3,24 @@
  * @param {string} magazine
  * @return {boolean}
  */
-function canConstruct(ransomNote, magazine) {
-  let basicWord = ransomNote;
-  let nextWord = magazine;
-   
-  for (let i = 0; i < basicWord.length; i++) {
-    if (nextWord.includes(basicWord[i])) {
-      nextWord = nextWord.replace(basicWord[i], "");
-      continue;
-    }
-     
-    return false;
-  }
+var canConstruct = function(ransomNote, magazine) {
+    const map = new Map();
 
-  return true;
-}
+    for (let i = 0; i < ransomNote.length; i++) {
+        map.set(ransomNote[i], (map.get(ransomNote[i]) || 0) + 1);
+    }
+
+    for (let i = 0; i < magazine.length; i++) {
+        const value = map.get(magazine[i]);
+
+        if (value) {
+            if (value === 1) {
+                map.delete(magazine[i]);
+            } else {
+                map.set(magazine[i], map.get(magazine[i]) - 1);
+            }
+        }
+    }
+
+    return map.size === 0 ? true : false;
+};
