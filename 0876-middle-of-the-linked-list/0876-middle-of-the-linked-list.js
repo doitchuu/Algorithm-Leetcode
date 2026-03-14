eeb83b603f4915f0966e346fe97c726bcc9f26ff
doitchuu <seulgichu14@gmail.com>
2026-03-14 18:41:31 +0900

    Time: 0 ms (100%), Space: 53.8 MB (50.48%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let index = 1;
+    let current = head;
+    const map = new Map();
+
+    while (current) {
+        map.set(index, current);
+        current = current.next;
+        index++;
+    }
+
+    map.set(index, current);
+
+    let middle = Math.ceil(map.size / 2);
+    // middle = middle % 2 === 0 ? middle + 1 : middle;
+
+    return map.get(middle);
+};
