fca52c72603907bcf2e830c79c9e3364095030a3
doitchuu <seulgichu14@gmail.com>
2026-03-15 14:53:26 +0900

    Time: 1 ms (46.61%), Space: 58.3 MB (47.03%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -10,17 +10,13 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) { 
-    function dfs (node) {
-        if (!node) {
-            return 0;
-        }
-
-        const left = dfs(node.left);
-        const right = dfs(node.right);
-
-        return Math.max(left, right) + 1;
+var maxDepth = function(root) {
+    if (!root) {
+        return 0;
     }
 
-    return dfs(root);
+    const left = maxDepth(root.left);
+    const right = maxDepth(root.right);
+
+    return Math.max(left, right) + 1;
 };
