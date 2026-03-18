b4cc515a32afad0fe6b53ad381aad209664c0bc6
doitchuu <seulgichu14@gmail.com>
2026-03-18 18:00:29 +0900

    Time: 21 ms (38.35%), Space: 73.9 MB (60.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,17 +2,9 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-function containsDuplicate(nums) {
-  const sortedNumber = nums.slice().sort((a,b) => a - b);
-  let basicWord = sortedNumber[0];
-  
-  for (let i = 1; i < sortedNumber.length; i++) {   
-    if (basicWord === sortedNumber[i]) {
-      return true;
-    }
-    
-    basicWord = sortedNumber[i];
-  }
-  
-  return false;
+var containsDuplicate = function(nums) {
+    const arrLength = nums.length;
+    const set = new Set(nums);
+
+    return arrLength !== set.size;
 };
